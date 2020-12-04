2bd5cfe5d632f436caec04603191d6d334a03b78
Yashasvi Bhatt <yashasvibhatt9@gmail.com>
2020-12-04 20:25:02 +0530

    Added K-Nearest_Neighbor

---
+++ 09_K_Nearest_Neighbor/k_nearest_neighbor.py
@@ -0,0 +1,32 @@
+# Importing the Libraries
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# Importing the dataset
+dF = pd.read_csv('Social_Network_Ads.csv')
+# Separating Features and Class
+X = dF.iloc[:, 2:-1].values
+y = dF.iloc[:, 4].values
+
+# Splitting the dataset into training set and testing set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# Fitting K-Nearest Neighbor Model to Training Set
+classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+classifier.fit(X_train, y_train)
+
+# Predicting Test Set Results
+y_pred = classifier.predict(X_test)
+
+# Evaluating Test Set Results
+cm = confusion_matrix(y_test, y_pred)
+# print(((cm[0][0] + cm[1][1]) / (cm[0][0] + cm[0][1] + cm[1][0] + cm[1][1])))
+print(accuracy_score(y_test, y_pred))
