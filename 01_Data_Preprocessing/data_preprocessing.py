4aa50729d3c7255b0f069eeeac7fbe61ee11c88c
Yashasvi Bhatt <yashasvibhatt9@gmail.com>
2021-10-06 10:03:49 +0530

    Code Optimization

--- 01_Data_Preprocessing/data_preprocessing.py
+++ 01_Data_Preprocessing/data_preprocessing.py
@@ -1,86 +1,59 @@
-# data preprocessing
-
 # Importing the Libraries
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
-# Importing the Dataset
+# Importing Data Set
 dF = pd.read_csv('Data.csv')
+print(dF.head())
 
-# Creating Data Frames of Features and Class
+# Splitting Feature Set and Class Set
 X = dF.iloc[:, :-1].values
 y = dF.iloc[:, 3].values
 
-# print(X)
-# print(y)
-
 # Managing missing data in dataset
-# Importing the Library required for managing missing data
-from sklearn.preprocessing import Imputer
+imputer1 = Imputer(missing_values='NaN', strategy='mean', axis=0)               # Instant for Mean Methodology
+imputer2 = Imputer(missing_values='NaN', strategy='most_frequent', axis=0)      # Instant for Mode Methodology
 
-imputer1 = Imputer(missing_values='NaN', strategy='mean', axis=0)
-imputer2 = Imputer(missing_values='NaN', strategy='most_frequent', axis=0)
+# Salary Column
 
-'''
-# Simply if we want to use same strategy on every column where the missing data is present we can use this :
-imputer = Imputer(missing_values='NaN', strategy='median', axis=0)
-X[:, 1:3] = imputer.fit_transform(X[:, 1:3])
-print(X)
-'''
+# Transforming Data
+x1 = X[:, 2].reshape(-1, 1)
+x1 = imputer1.fit_transform(x1)
+x1 = x1.reshape(1, -1)
 
-# But if we want to use different strategies on different columns we do this
+# Replacing Missing Value Containing Feature with Converted Feature
+X = np.delete(X, 2, 1)
+X = np.insert(X, 2, x1, axis = 1)
 
-# for salary column
-x1 = X[:, 2].reshape(-1, 1)                             # creating a new reshaped multi-dimensional array from 1D array
-x1 = imputer1.fit_transform(x1)                         # fitting and transforming data
-# print(x1)
-x1 = x1.reshape(1, -1)                                  # restoring to 1D array after missing data management
-X = np.delete(X, 2, 1)                                  # deleting the column with missing values
-X = np.insert(X, 2, x1, axis = 1)                       # inserting the column with no missing values
-# print(X)
+# Age Column
 
-# for age column
-x2 = X[:, 1].reshape(-1, 1)                             # creating a new reshaped multi-dimensional array from 1D array
+# Transforming Data
+x2 = X[:, 1].reshape(-1, 1)
 x2 = imputer2.fit_transform(x2)
-# print(x2)
-x2 = x2.reshape(1, -1)                                  # restoring to 1D array after missing data management
-X = np.delete(X, 1, 1)                                  # deleting the column with missing values
-X = np.insert(X, 1, x2, axis = 1)                       # inserting the column with no missing values
-# print(X)
+x2 = x2.reshape(1, -1)
 
-# Encoding dataframe X
-# Importing Library/Class
-from sklearn.preprocessing import LabelEncoder
-labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-# print(X)
+# Replacing Missing Value Containing Feature with Converted Feature
+X = np.delete(X, 1, 1)
+X = np.insert(X, 1, x2, axis = 1)
 
-# Importing Library/Class
-from sklearn.preprocessing import OneHotEncoder
-onehotencoder_X = OneHotEncoder(categorical_features = [0])
-X = onehotencoder_X.fit_transform(X).toarray()
-# toarray() is used to convert output to array type
-print(X)
+# Label Encoding
+labelencoder = LabelEncoder()                       # Intantiating LabelEncoder
+X[:, 0] = labelencoder.fit_transform(X[:, 0])       # Encoding Feature Sets
+y = labelencoder.fit_transform(y)                   # Encoding Class Set
 
-# Encoding dataframe y
-labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)
-print(y)
+# One Hot Encoding
+onehotencoder_X = OneHotEncoder(categorical_features = [0])     # Intantiating OneHotEncoder
+X = onehotencoder_X.fit_transform(X).toarray()                  # Encoding Data
 
-# Splitting the Data into training set and testing set
-# Importing Library
-from sklearn.model_selection import train_test_split
+# Splitting data into Training Set and Testing Set
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.80)
-# print(X_train)
-# print(X_test)
-# print(y_train)
-# print(y_test)
 
 # Feature Scaling
-# Importing libraries
-from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler(with_mean = False)
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
-print(X_train)
-print(X_test)
